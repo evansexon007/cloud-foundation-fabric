9735a6aadeafeb3386f0824079ff341145fbbd03
evansexon007 <38249329+evansexon007@users.noreply.github.com>
2026-02-24 16:39:17 +0100

    rere

--- Environments/Production/vm.tf
+++ Environments/Production/vm.tf
@@ -41,3 +41,47 @@ module "linux_vm_01" {
   ]
 
 }
+
+module "linux_vm_02" {
+  source = "../../modules/compute-vm"
+
+  project_id = "myproject-testsexon-01"
+  name       = "linux-vm-02"
+  zone       = "europe-west2-b"
+
+  # Small machine
+  instance_type = "e2-micro"
+
+  # Boot disk (Debian)
+  boot_disk = {
+    initialize_params = {
+      image = "projects/debian-cloud/global/images/family/debian-12"
+      size  = 10
+      type  = "pd-balanced"
+    }
+  }
+
+  # Attach to your subnet
+  network_interfaces = [
+    {
+      # ✅ Shared VPC: use SELF LINKS owned by HOST project
+      network    = "projects/myproject-prod-01/global/networks/vpc-main"
+      subnetwork = "projects/myproject-prod-01/regions/europe-west2/subnetworks/subnet-test-01"
+
+      nat = false
+    }
+  ]
+
+  # Optional but recommended
+  tags = ["linux", "ssh"]
+
+  service_account = {
+    email  = google_service_account.web_sa.email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  depends_on = [
+    module.vpc_main
+  ]
+
+}
