cbd2f6ba6aba18827ac533d940dabe1143b02247
evansexon007 <38249329+evansexon007@users.noreply.github.com>
2026-02-26 12:39:45 +0100

    fff

--- Environments/Production/dns-host.tf
+++ Environments/Production/dns-host.tf
@@ -129,65 +129,65 @@ module "dns_forward_rws_local" {
 ###
 
 
-module "dns_googleapis_private_hub" {
-  source = "../../modules/dns"
+#module "dns_googleapis_private_hub" {
+ # source = "../../modules/dns"
+#
+ # project_id    = "myproject-standalone"
+ # name          = "pz-googleapis"
+ # description   = "Private zone override for googleapis.com -> private.googleapis.com (PGA)"
+ # force_destroy = false
 
-  project_id    = "myproject-standalone"
-  name          = "pz-googleapis"
-  description   = "Private zone override for googleapis.com -> private.googleapis.com (PGA)"
-  force_destroy = false
+ # zone_config = {
+ #   domain = "googleapis.com."
+#
+#    private = {
+#      client_networks = [
+#        module.vpc_main_standalone.self_link
+#      ]
+#    }
+#  }
 
-  zone_config = {
-    domain = "googleapis.com."
+#  recordsets = {
+#    # A records for the PGA VIPs
+#    "A private" = {
+#      ttl     = 300
+#      records = ["199.36.153.8", "199.36.153.9", "199.36.153.10", "199.36.153.11"]
+#    }
+#
+#    # Wildcard all Google APIs to private.googleapis.com
+#    "CNAME *" = {
+#      ttl     = 300
+#      records = ["private.googleapis.com."]
+#    }
+#  }
 
-    private = {
-      client_networks = [
-        module.vpc_main_standalone.self_link
-      ]
-    }
-  }
+#  depends_on = [
+#    google_project_service.dns
+#  ]
+#}
 
-  recordsets = {
-    # A records for the PGA VIPs
-    "A private" = {
-      ttl     = 300
-      records = ["199.36.153.8", "199.36.153.9", "199.36.153.10", "199.36.153.11"]
-    }
+#module "dns_peer_googleapis_to_hub" {
+#  source = "../../modules/dns"
 
-    # Wildcard all Google APIs to private.googleapis.com
-    "CNAME *" = {
-      ttl     = 300
-      records = ["private.googleapis.com."]
-    }
-  }
+#  project_id  = var.host_project_id
+#  name        = "peer-googleapis-to-hub"
+#  description = "Peering zone so Shared VPC can resolve googleapis.com via Hub DNS"
 
-  depends_on = [
-    google_project_service.dns
-  ]
-}
+#  zone_config = {
+#    domain = "googleapis.com."
+##    peering = {
+#      client_networks = [
+#        module.vpc_main.self_link
+#      ]
+#      peer_network = module.vpc_main_standalone.self_link
+#    }
+#  }
 
-module "dns_peer_googleapis_to_hub" {
-  source = "../../modules/dns"
+#  recordsets = {}
 
-  project_id  = var.host_project_id
-  name        = "peer-googleapis-to-hub"
-  description = "Peering zone so Shared VPC can resolve googleapis.com via Hub DNS"
-
-  zone_config = {
-    domain = "googleapis.com."
-    peering = {
-      client_networks = [
-        module.vpc_main.self_link
-      ]
-      peer_network = module.vpc_main_standalone.self_link
-    }
-  }
-
-  recordsets = {}
-
-  depends_on = [
-    google_project_service.dns2,
-    module.dns_googleapis_private_hub
-    # or module.dns_googleapis_restricted_private_hub if you used restricted
-  ]
-}
+#  depends_on = [
+#    google_project_service.dns2,
+#    module.dns_googleapis_private_hub
+#    # or module.dns_googleapis_restricted_private_hub if you used restricted
+#  ]
+#}
