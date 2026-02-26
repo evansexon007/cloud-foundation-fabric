7172b03193d9e8b7cd36093e7387ef48aaf1b88d
evansexon007 <38249329+evansexon007@users.noreply.github.com>
2026-02-26 12:26:34 +0100

    psc

--- Environments/Production/vpc.tf
+++ Environments/Production/vpc.tf
@@ -71,15 +71,32 @@ resource "google_project_service" "compute_standalone" {
   disable_on_destroy = false
 }
 
+resource "google_compute_subnetwork" "psc_nat_subnet" {
+  project       = "myproject-standalone"
+  name          = "subnet-psc-nat"
+  region        = "europe-west2"
+  network       = module.vpc_main_standalone.self_link
+  ip_cidr_range = "10.250.0.0/24"
+
+  purpose = "PRIVATE_SERVICE_CONNECT"
+  role    = "ACTIVE"
+
+  depends_on = [
+    google_project_service.compute_standalone
+  ]
+}
+
 resource "google_compute_global_address" "psc_googleapis_ip" {
   project      = "myproject-standalone"
   name         = "psc-googleapis-ip"
   address_type = "INTERNAL"
   purpose      = "PRIVATE_SERVICE_CONNECT"
   network      = module.vpc_main_standalone.self_link
-  address      = "10.220.0.50"
+
+  # choose an IP inside the PSC subnet range you created above
+  address = "10.250.0.10"
 
   depends_on = [
-    google_project_service.compute_standalone
+    google_compute_subnetwork.psc_nat_subnet
   ]
 }
