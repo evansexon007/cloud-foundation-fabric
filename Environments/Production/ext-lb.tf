33ac0ed0fa261f74c886ef354f38c7392fa9f0eb
evansexon007 <38249329+evansexon007@users.noreply.github.com>
2026-02-26 16:49:22 +0100

    simple lb

---
+++ Environments/Production/ext-lb.tf
@@ -0,0 +1,38 @@
+module "ext_lb_app" {
+  source     = "../../modules/net-lb-app-ext"
+  project_id = "myproject-standalone"
+  name       = "app-ext-lb"
+
+  # Define the two unmanaged instance groups already created above:
+  group_configs = {
+    linux-a = {
+      name        = "uig-linux-a"
+      project_id  = "myproject-testsexon-01"
+      zone        = "europe-west2-a"
+      instances   = [
+        "projects/myproject-testsexon-01/zones/europe-west2-a/instances/linux-vm-01"
+      ]
+      named_ports = { http = 80 }
+    }
+
+    linux-b = {
+      name        = "uig-linux-b"
+      project_id  = "myproject-testsexon-01"
+      zone        = "europe-west2-b"
+      instances   = [
+        "projects/myproject-testsexon-01/zones/europe-west2-b/instances/linux-vm-02"
+      ]
+      named_ports = { http = 80 }
+    }
+  }
+
+  # Create one default backend service pointing at both groups:
+  backend_service_configs = {
+    default = {
+      backends = [
+        { backend = "linux-a" },
+        { backend = "linux-b" }
+      ]
+    }
+  }
+}
